c4d735874d044493fceae6daf6cc7f7616cad028
Evan De la Garza <eadlg2@tamu.edu>
2022-11-05 12:05:31 -0500

    Updated table to use react-table-library

--- src/routes/manage/components/ManageInventory.js
+++ src/routes/manage/components/ManageInventory.js
@@ -2,42 +2,49 @@ import { HeaderMedium } from "../../../components/typography/Header";
 import axios from 'axios'
 import { SERVER_URL } from '../../../constants'
 import { useQuery } from 'react-query'
+import { CompactTable } from '@table-library/react-table-library/compact';
+import { useTheme } from '@table-library/react-table-library/theme'
+import { DEFAULT_OPTIONS, getTheme } from '@table-library/react-table-library/material-ui'
 
 function ManageInventory() {
+  const columns = [
+    {label: 'Name', renderCell: (item) => item.item_name},
+    {label: 'Amount', renderCell: (item) => item.item_amount},
+    {label: 'Storage Location', renderCell: (item) => item.storage_location.charAt(0).toUpperCase() + item.storage_location.slice(1)},
+  ]
+
   const fetchIngredients = async () => {
     return await axios.get(`${SERVER_URL}/ingredients`)
   }
 
-  const { data: ingredientsData } = useQuery(
+  const { data } = useQuery(
     'ingredients',
     fetchIngredients,
   )
-  
-  if (ingredientsData) {
+
+  const materialTheme = getTheme(DEFAULT_OPTIONS)
+  const customTheme = {
+    HeaderRow: `
+      background-color: var(--secondary);
+      color: var(--white);
+    `,
+    Row: `
+      &:nth-child(odd) {
+        background-color: var(--gray-0);
+      }
+    `,
+  }
+  const theme = useTheme([materialTheme, customTheme])
+
+  if (data) {
+    const ingredientsData = { nodes: data.data }
+
     return (
       <>
         <HeaderMedium>Inventory</HeaderMedium>
-      
+
         <div className="manage-inventory-table">
-          <table>
-            <tr>
-              <th>Name</th>
-              <th>Amount</th>
-              <th>Storage Location</th>
-            </tr>
-            {ingredientsData
-              .data
-              .map((ingredient) => {
-                return (
-                  <tr key={ingredient}>
-                    <td>{ingredient.item_name}</td>
-                    <td>{ingredient.item_amount}</td>
-                    <td>{ingredient.storage_location}</td>
-                  </tr>
-                )
-              }
-            )}
-          </table>
+          <CompactTable columns={columns} data={ingredientsData} theme={theme} layout={{ fixedHeader: true }} />
         </div>
       </>
     )
