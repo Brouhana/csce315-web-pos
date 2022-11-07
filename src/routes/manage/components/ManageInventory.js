14e9bae0d9552ae70af64290a9a060eca5108661
Evan De la Garza <eadlg2@tamu.edu>
2022-11-06 20:59:03 -0600

    Added ability to add item to ingredients and search ingredients table

--- src/routes/manage/components/ManageInventory.js
+++ src/routes/manage/components/ManageInventory.js
@@ -5,8 +5,22 @@ import { useQuery } from 'react-query'
 import { CompactTable } from '@table-library/react-table-library/compact';
 import { useTheme } from '@table-library/react-table-library/theme'
 import { DEFAULT_OPTIONS, getTheme } from '@table-library/react-table-library/material-ui'
+import { usePagination } from '@table-library/react-table-library/pagination';
+import { Stack, TablePagination, TextField } from '@mui/material';
+import { useState } from "react";
+import Button from "../../../components/Button";
+import { FaSearch } from 'react-icons/fa';
 
 function ManageInventory() {
+  const [ isLoading, setIsLoading ] = useState(false)
+  const [ isRendering, setIsRendering ] = useState(false)
+
+  const [ nameInput, setNameInput ] = useState("")
+  const [ amountInput, setAmountInput ] = useState(0)
+  const [ storageInput, setStorageInput ] = useState("")
+
+  const [ ingredientsData, setIngredientsData ] = useState({ nodes: [] })
+
   const columns = [
     {label: 'Name', renderCell: (item) => item.item_name},
     {label: 'Amount', renderCell: (item) => item.item_amount},
@@ -26,7 +40,6 @@ function ManageInventory() {
   const customTheme = {
     Table: `
       --data-table-library_grid-template-columns: 50% 25% 25%;
-      height: 480px;
     `,
     HeaderRow: `
       background-color: var(--secondary);
@@ -40,24 +53,142 @@ function ManageInventory() {
   }
   const theme = useTheme([materialTheme, customTheme])
 
-  if (ingredientsRes) {
-    const ingredientsData = { nodes: ingredientsRes.data }
+  const [ search, setSearch ] = useState("")
 
-    return (
-      <>
-        <HeaderMedium>Inventory</HeaderMedium>
+  const handleSearch = (event) => {
+    setSearch(event.target.value);
 
-        <div className="manage-table">
-          <CompactTable 
-            columns={columns} 
-            data={ingredientsData} 
-            theme={theme} 
-            layout={{ custom: true, fixedHeader: true }} 
-          />
-        </div>
-      </>
-    )
+    setIngredientsData({ 
+      nodes: ingredientsRes.data.filter((item) => 
+        item.item_name.toLowerCase().includes(search.toLowerCase())
+      ) 
+    })
   }
+
+  const pagination = usePagination(ingredientsData, {
+    state: {
+      page: 0,
+      size: 7,
+    },
+    onChange: onPaginationChange,
+  })
+
+  function onPaginationChange(action, state) {
+    console.log(action, state);
+  }
+
+  async function updateInventory() {
+    setIsLoading(true)
+
+    if (nameInput && amountInput && storageInput) {
+      await axios.post(`${SERVER_URL}/ingredients`, {
+        id: 0,
+        item_name: nameInput,
+        item_amount: parseInt(amountInput, 10),
+        storage_location: storageInput
+      })
+
+      setIsLoading(false)
+    }
+  }
+
+  async function renderTable() {
+    setIsRendering(true)
+
+    let ingredientsRes = await axios.get(`${SERVER_URL}/ingredients`)
+
+    if (ingredientsRes) {
+      setIngredientsData({ nodes: ingredientsRes.data })
+      pagination.fns.onSetPage(0)
+      setIsRendering(false)
+    }
+  }
+
+  return (
+    <>
+      <HeaderMedium>Inventory</HeaderMedium>
+
+      <Stack spacing={10}>
+        <TextField label="Search Ingredients" value={search} icon={<FaSearch />} onChange={handleSearch} size="small" />
+      </Stack>
+
+      <br />
+
+      <div className="manage-table">
+        <CompactTable 
+          columns={columns} 
+          data={ingredientsData} 
+          theme={theme} 
+          layout={{ custom: true, fixedHeader: true }} 
+          pagination={pagination} 
+        />
+      </div>
+
+      <br />
+        <Stack spacing={10}>
+          <TablePagination
+            count={ingredientsData.nodes.length}
+            page={pagination.state.page}
+            rowsPerPage={pagination.state.size}
+            rowsPerPageOptions={[1, 2, 5, 7]}
+            onRowsPerPageChange={(event) =>
+              pagination.fns.onSetSize(parseInt(event.target.value, 10))
+            }
+            onPageChange={(event, page) => pagination.fns.onSetPage(page)}
+          />
+        </Stack>
+      <br />
+
+      <Stack direction="row" spacing={2}>
+        <TextField 
+          id="name-field"
+          label="Name"
+          fullWidth
+          placeholder={"Enter Name"}
+          InputLabelProps={{ shrink: true }}
+          onChange={(event) => setNameInput(event.target.value)}
+        />
+
+        <TextField 
+          id="amount-field"
+          label="Amount"
+          fullWidth
+          placeholder={"Enter Amount"}
+          InputLabelProps={{ shrink: true }}
+          onChange={(event) => setAmountInput(event.target.value)}
+        />
+
+        <TextField 
+          id="storage-field"
+          label="Storage Location"
+          fullWidth
+          placeholder={"Warm/Cold"}
+          InputLabelProps={{ shrink: true }}
+          onChange={(event) => setStorageInput(event.target.value)}
+        />
+
+        <Button
+          size="lg"
+          fullWidth
+          isLoading={isLoading}
+          onClick={() => updateInventory()}
+        >
+          Add Ingredient
+        </Button>
+      </Stack>
+
+      <br />
+
+      <Button
+        size="md"
+        fullWidth
+        isLoading={isRendering}
+        onClick={() => renderTable()}
+      >
+        Render Table
+      </Button>
+    </>
+  )
 }
 
 export default ManageInventory
